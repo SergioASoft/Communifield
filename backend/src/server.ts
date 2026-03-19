91913f857f8d70dac97068f181b47c7df0182372
SergioASoft <sergioas2007@gmail.com>
2026-03-19 16:30:40 -0500

    se sube parte del módulo de autenticación creada

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import userRoutes from "./routes/userRoutes"
+import authRoutes from "./modules/auth.routes"
+
 
 
 dotenv.config()
@@ -11,6 +13,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use("/users", userRoutes)
+app.use("/auth", authRoutes)
+
 
 app.get("/", (req, res) => {
     res.send("CommuniField API running")
